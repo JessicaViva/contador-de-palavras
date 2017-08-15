dfef3573eb3d73e210a9a727985754584b1910d5
JessicaViva <jessirviva@gmail.com>
2017-08-14 23:00:57 -0300

    Exercicio Certo

--- src/main.c
+++ src/main.c
@@ -6,17 +6,47 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main() {
 
-  char c;
+  char c[500];
+  int i, count=0, j=0;
+  int tam (char c[]);
 
-  c = 1;
+  fgets(c,500,stdin);
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+  j= tam(c);
 
-  printf("1\n");
-  return 0;
+  for(i=0; i<=j; i++){
+     
+    if((c[i+1]<'0')||((c[i+1]>'9') && (c[i+1]<'A'))||((c[i+1]>'Z') && (c[i+1]<'a'))||(c[i+1]>'z')){
+      if((c[i]>='0')&&(c[i]<='9')){
+      	count++;
+      }
+      if((c[i]>='A')&&(c[i]<='Z')){
+	count++;
+      }
+      if((c[i]>='a')&&(c[i]<='z')){
+        count++;
+      }
+    }
+
+ }
+printf("%d\n", count);
+return 0;
+
 }
+
+int tam (char c[]){
+  int i, n=0;
+     for (i=0;c[i]!='\0';i++) {
+           n++;
+         }
+    return n;
+}
+  
+
+ 
